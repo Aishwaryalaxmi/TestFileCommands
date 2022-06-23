83ecc3c1ef92f55c28d1b77a53ccf94757626ab9
Aishwaryalaxmi <aishurao199@gmail.com>
2022-06-23 13:00:39 +0530

    Update publish_to_axiom.sh

--- publish_to_axiom.sh
+++ publish_to_axiom.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-curl \
-  -X POST \
-  -H "Accept:application/json" \
-  -H "Content-Type: multipart/form-data" \
-  -F "key=${1}" \
-  -F "projectId=${2}" \
-  -F "folderId=33" \
-  -F "jobId=${3}" \
-  -F "commitMessage=${4}" \
-  -F "file=@${5}" \
-  https://axiom-api-beta.ymedia.in/upload/build
+http_response=$(curl -s -o response.txt -w "%{http_code}" http://example.com)
+if [ $http_response != "200" ]; then
+    # handle error
+else
+    echo "Server returned:"
+    cat response.txt    
+fi
