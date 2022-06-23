72bccfdd1c186644de69cefcd58adb7784fca765
Aishwaryalaxmi <aishurao199@gmail.com>
2022-06-23 13:04:19 +0530

    Update publish_to_axiom.sh

--- publish_to_axiom.sh
+++ publish_to_axiom.sh
@@ -2,7 +2,7 @@
 
 http_response=$(curl -s -o response.txt -w "%{http_code}" http://example.com)
 if [ $http_response != "200" ]; then
-    # handle error
+    echo "Server returned error:"
 else
     echo "Server returned:"
     cat response.txt    
